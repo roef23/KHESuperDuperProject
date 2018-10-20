baec7daea3b9354294d64a3f805a00181760614c
Ryan Akuszewski <ryan.akuszewski@gmail.com>
2018-10-19 23:02:17 -0400

    Added a file!

---
+++ cmake-build-debug/CMakeFiles/KHE2018SuperDuperProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KHE2018SuperDuperProject.dir/main.cpp.obj"
+  "KHE2018SuperDuperProject.pdb"
+  "KHE2018SuperDuperProject.exe"
+  "KHE2018SuperDuperProject.exe.manifest"
+  "libKHE2018SuperDuperProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KHE2018SuperDuperProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/KHE2018SuperDuperProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for KHE2018SuperDuperProject.
+# This may be replaced when dependencies are built.
